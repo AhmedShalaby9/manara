88c3af261adf6a770d4dfec40bbe145887e19ccd
Ahmed Gamal Shalaby <shalabi.dev@gmail.com>
2025-12-30 14:42:51 +0200

    preload user in student object

--- controllers/student_controller.go
+++ controllers/student_controller.go
@@ -24,7 +24,7 @@ func GetStudents(c *gin.Context) {
 		query = query.Where("grade_level =?", gradeLevel)
 	}
 
-	res := query.Find(&students).Preload("User")
+	res := query.Preload("User").Find(&students)
 	if res.Error != nil {
 		helpers.Respond(c, false, nil, res.Error.Error())
 		return
